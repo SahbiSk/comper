70aa14649efd6dfef6e8bce4215ff9fff9e5ede0
ghassen <ghassenbriki123@gmail.com>
2021-04-26 00:23:50 +0100

    favourites API

--- server/controllers/productCtrl.js
+++ server/controllers/productCtrl.js
@@ -1,14 +1,46 @@
+const { array } = require('../middlewares/multerConfig')
 const product=require('../models/product')
 
+let errors={name:'',description:'',category:'',price:''}
+
+
+const handleError=(err)=>
+{
+   if(err.message.includes("Product validation failed"))
+   Object.values(err.errors).forEach((error)=>
+   {
+    errors[error.properties.path]=error.properties.message
+   })
+
+   return errors
+}
 
 
 exports.addProd=(req,res,next)=>
 {
+    console.log("fsdfs")
+    let img=[]
+
+    if (req.files){
+       req.files.forEach(el=> {
+            img.push(el.path)            
+        });
+       
+    }
+    
+  
+    req.body.owner=req.user._id
+    req.body.images=img
     product.create(req.body).then(doc=> {
-        
-        res.status(201).json({doc:doc})
-    }).catch(err=>  res.status(401).json({err:err}))
+      
+        res.status(201).json({doc})
+    }).catch(err=>  {console.log(err),res.status(401).json({err:handleError(err)})})
 
 
 }
 
+
+exports.getProd=(req,res)=>
+{
+    product.find({}).then((doc)=> res.status(200).json(doc)).catch(err=>res.send(err))
+}
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,9 +1,14 @@
 const express=require('express');
 const productRouter=express.Router()
 const produtCtrl=require('../controllers/productCtrl')
+const auth=require('../middlewares/authenticate')
+const upload=require('../middlewares/multerConfig')
 
 
-productRouter.post('/',produtCtrl.addProd)
+
+productRouter.post('/',auth,upload.array('productImages'),produtCtrl.addProd)
+productRouter.get('/',produtCtrl.getProd)
+
 
 
 
